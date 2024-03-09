66fb6e5ac6d5ebdcad02336ddee6b8ede574ee5a
sabahAlmel <142877822+sabahAlmel@users.noreply.github.com>
2024-03-09 18:39:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000,https://ctra.vercel.app/",
     credentials: true,
     optionsSuccessStatus: 200,
   })
